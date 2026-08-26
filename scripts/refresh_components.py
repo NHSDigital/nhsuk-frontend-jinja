ae71180f122990353ad27a4d8c781f1e96a771e1
Colin Rotherham <work@colinr.com>
2026-08-26 16:07:56 +0100

    Apply ruff formatting

--- scripts/refresh_components.py
+++ scripts/refresh_components.py
@@ -26,7 +26,9 @@ JINJA_EXT = ".jinja"
 
 UNQUOTED_KEY = re.compile(r"^(?P<leading_space>\s*)(?P<name>\w+): ")
 INLINE_UNQUOTED_KEY = re.compile(r"(?P<prefix>[{,]\s*)(?P<name>[A-Za-z]\w*)\s*:")
-IS_MAPPING = re.compile(r"\b(?P<params>[A-Za-z\.]*) is mapping and (?P=params) is not escaped\b")
+IS_MAPPING = re.compile(
+    r"\b(?P<params>[A-Za-z\.]*) is mapping and (?P=params) is not escaped\b"
+)
 ITEMS = re.compile(r"\b(?P<params>[A-Za-z\.]*)\.(?P<property>items|values)\b(?!\s*\()")
 ITEMS_GET = re.compile(r"\bif (?P<params>[A-Za-z]+\.[A-Za-z\.]+)\.get\(\"")
 
@@ -44,10 +46,7 @@ def standard_macro_replacements(filepath, accepts_caller=False):
 
             file.write(line)
 
-            if (
-                accepts_caller
-                and line.lstrip().startswith("{% macro ")
-            ):
+            if accepts_caller and line.lstrip().startswith("{% macro "):
                 file.write("  {%- if caller -%}\n")
                 file.write("  {# noop for Jinja support #}\n")
                 file.write("  {%- endif -%}\n")
